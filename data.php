cb2135eb6e628338d0a858671fbe13eec43d1605
Abid Al-Wasi <abidalwasi@outlook.com>
2021-05-14 00:20:35 +0600

    updated project

--- data.php
+++ data.php
@@ -5,7 +5,7 @@ $result = mysqli_query($conn, "SELECT * FROM employees");
 $data = array();
 while ($row = mysqli_fetch_object($result))
 {
-    array_push($data, $row);
+  array_push($data, $row);
 }
 
 echo json_encode($data);
